c7462cfb4cc4b360f1004b572a9a8c1a013e42e4
akhtartatariya <akhtartatariya19@gmail.com>
2023-09-18 12:30:29 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,83 @@
+const question_data = [
+  {
+    question: "What does CPU stand for in the context of computing?",
+    a: "Central Processing Unit",
+    b: "Computer Personal Unit",
+    c: "Central Process Unit",
+    d: "Computer Power Unit",
+    correct: "a"
+  },
+  {
+    question: "Which programming language is commonly used for developing web-based applications?",
+    a: "C++",
+    b: "Java",
+    c: "Python",
+    d: "JavaScript",
+    correct: "d"
+  },
+  {
+    question: "What does HTML stand for in web development?",
+    a: "Hyper Transfer Markup Language",
+    b: "High Text Modern Language",
+    c: "Hyperlink and Text Markup Language",
+    d: "Hypertext Markup Language",
+    correct: "d"
+  },
+  {
+    question: "Which of the following is a popular version control system used by developers to track changes in code?",
+    a: "SQL",
+    b: "NoSQL",
+    c: "Git",
+    d: "HTML",
+    correct: "c"
+  },
+  {
+    question: "Which protocol is used for sending emails over the internet?",
+    a: "FTP",
+    b: "HTTP",
+    c: "SMTP",
+    d: "POP3",
+    correct: "c"
+  },
+  {
+    question: "What does RAM stand for in computer terms?",
+    a: "Random Access Memory",
+    b: "Read-Only Memory",
+    c: "Rapid Application Management",
+    d: "Remote Access Module",
+    correct: "a"
+  },
+  {
+    question: "Which of the following is not a programming language?",
+    a: "Python",
+    b: "Ruby",
+    c: "SQL",
+    d: "URL",
+    correct: "d"
+  },
+  {
+    question: "What is the primary function of an operating system?",
+    a: "Managing hardware components",
+    b: "Running applications",
+    c: "Providing internet connectivity",
+    d: "Managing office documents",
+    correct: "a"
+  },
+  {
+    question: "Which data structure follows the Last-In-First-Out (LIFO) principle?",
+    a: "Queue",
+    b: "Stack",
+    c: "Linked List",
+    d: "Tree",
+    correct: "b"
+  },
+  {
+    question: "What does IP stand for in the context of networking?",
+    a: "Internet Protocol",
+    b: "Information Processing",
+    c: "Interconnection Protocol",
+    d: "Internet Provider",
+    correct: "a"
+  }
+];
+
